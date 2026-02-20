ce40365da4811eded570436862e472adf8b87370
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-02-20 12:45:37 +0530

    minor chnages

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -288,7 +288,7 @@ const AdminDashboard = () => {
                             ))}
                         </div>
                     </div>
-
+                    
                     {/* Cards Grid */}
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                         {filteredInventory.length > 0 ? (
